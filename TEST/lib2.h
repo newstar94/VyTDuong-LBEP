a16c0c4d7664b3bc119e9d05f61a22c448226008
newstar94 <77623864+newstar94@users.noreply.github.com>
2021-06-05 16:02:44 +0700

    del

--- TEST/lib2.h
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-void upperCase(char s[]){
-	for (int i = 0; i < strlen(s); i++) {
-        if (s[i] >= 'a' && s[i] <= 'z') {
-            s[i] -= 32;
-        }
-    }
-}
